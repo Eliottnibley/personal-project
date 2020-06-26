d0e9ed94d0eb0b662580bb143a2916c20bfcd8ba
Eliottnibley <e.nibley@gmail.com>
2020-06-26 15:46:20 -0600

    update

--- src/components/Company/Company.js
+++ src/components/Company/Company.js
@@ -1,14 +1,48 @@
 import React from 'react'
 import './Company.css'
+import { connect } from 'react-redux'
+import {useHistory} from 'react-router-dom'
 
 function Company (props) {
-  return (
-    <div className='company-container'>
-      <div className='company-nav'>
-        
+  const history = useHistory()
+
+  if(!props.isLoggedIn){
+    history.push('/')
+  }
+  
+  if(props.user.isAdmin){
+    return (
+      <div className='company-container'>
+        <div className='company-nav'>
+          <div className='company-nav-menu'>
+            <ul>
+              <li>Chat Groups</li>
+              <li>Files</li>
+              <li>Projects</li>
+              <li>Admin</li>
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+  else {
+    return (
+      <div className='company-container'>
+        <div className='company-nav'>
+          <div className='company-nav-menu'>
+            <ul>
+              <li>Chat Groups</li>
+              <li>Files</li>
+              <li>Projects</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default Company
+const mapStateToProps = reduxState => reduxState
+
+export default connect(mapStateToProps)(Company)
