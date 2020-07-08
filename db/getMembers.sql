d277ec7d92b5cef0d296a1f861095ab0a338f77f
Eliottnibley <e.nibley@gmail.com>
2020-07-08 11:54:41 -0600

    update

--- db/getMembers.sql
+++ db/getMembers.sql
@@ -1,3 +1,4 @@
-select * from work_users
+select work_users.id, work_users.firstname, work_users.lastname, work_users.email, work_users.profile_pic, work_users.is_admin from company
+join work_users on work_users.company_id = company.id
 where company_id = $1
 order by lastname asc;
